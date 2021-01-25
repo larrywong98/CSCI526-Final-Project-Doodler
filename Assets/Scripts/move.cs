67432eff29009c81d0c187cbebb4e55e9f9060a1
unknown <larrywong9898@gmail.com>
2021-01-26 00:54:59 +0800

    version update

---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position=
+        transform.position+(Vector3)VJoystick.joystickpos*speed*Time.deltaTime;
+    
+    }
+}
