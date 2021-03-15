85d9295e811c9223b631a8b270c8ce40edb7ebbe
Yijie Yang <eyangpc@Yijies-MacBook-Pro.local>
2021-03-14 23:42:14 -0500

    修改内容

--- Assets/Scripts/WBC/WbcMovement.cs
+++ Assets/Scripts/WBC/WbcMovement.cs
@@ -11,62 +11,56 @@ public class WbcMovement : MonoBehaviour
     private float moveH, moveV;
     [SerializeField] private float moveSpeed;
     public Animator animator;
-
+    
+    public bool NormalMovement { get; set; }
+    public Vector3 CurrentMovement { get; set; }
 
 
     // Start is called before the first frame update
     void Start()
     {
+        NormalMovement = true;
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(VJoystick.joystickpos.x != 0 || VJoystick.joystickpos.y!=0 ){
-            moveH = VJoystick.joystickpos.x* moveSpeed;
-            moveV = VJoystick.joystickpos.y* moveSpeed;
-        }else{
-            moveH = Input.GetAxis("Horizontal") * moveSpeed;
-            moveV = Input.GetAxis("Vertical") * moveSpeed;
+        if (NormalMovement)
+        {
+            MoveCharacter();
         }
-        
+    }
 
-        Vector3 movement = new Vector3 (VJoystick.joystickpos.x,VJoystick.joystickpos.y, -1f);
-        // Vector3 movement = new Vector3 (Input.GetAxis("Horizontal"),Input.GetAxis("Vertical") , -1f);
+    private void Flip()
+    {
+        if (moveH > 0)
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        if (moveH < 0)
+            transform.eulerAngles = new Vector3(0, 0, 0);
+    }
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("AttackDirection", VAim.attackDirection.x);
+    public void MoveCharacter()
+    {
+        // moveH = VJoystick.joystickpos.x* moveSpeed;
+        // moveV = VJoystick.joystickpos.y* moveSpeed;
+        moveH = Input.GetAxis("Horizontal") * moveSpeed;
+        moveV = Input.GetAxis("Vertical") * moveSpeed;
+        // Vector3 movement = new Vector3 (VJoystick.joystickpos.x,VJoystick.joystickpos.y, -1f);
+        Vector3 movement = new Vector3 (Input.GetAxis("Horizontal"),Input.GetAxis("Vertical") , -1f);
+        CurrentMovement = movement;
         animator.SetFloat("Magnitude", movement.magnitude);
         Flip();
         rb.velocity = new Vector2(moveH, moveV);
     }
 
-    private void Flip()
+    public void SetMovement(Vector3 newPosition)
     {
-        // if (VJoystick.joystickpos.x > 0)
-        //     transform.eulerAngles = new Vector3(0, 180, 0);
-        // if (VJoystick.joystickpos.x < 0)
-        //     transform.eulerAngles = new Vector3(0, 0, 0);
-        
-        
-        
-        // if (VAim.isAttackButtionUp == 1){
-        //     if(VAim.attackDirection.x >= 0){
-        //         transform.eulerAngles = new Vector3(0, 180, 0);
-        //     }
-        //     if(VAim.attackDirection.x < 0){
-        //         transform.eulerAngles = new Vector3(0, 0, 0);
-        //     }
+        CurrentMovement = newPosition;
+    }
 
-        // }else{
-
-        //     if (moveH > 0)
-        //         transform.eulerAngles = new Vector3(0, 180, 0);
-        //     if (moveH < 0)
-        //         transform.eulerAngles = new Vector3(0, 0, 0);
-        // }
-        
-
+    public void MovePosition(Vector2 newPosition)
+    {
+        rb.MovePosition(newPosition);
     }
 }
