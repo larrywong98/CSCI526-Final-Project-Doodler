20a8c7b669620d1680b9ff88bef240dbf119c067
larrywong98 <larrywong9898@gmail.com>
2021-04-22 13:45:03 +0800

    add purple defender

--- Assets/Scripts/Enemy/purpledefender.cs
+++ Assets/Scripts/Enemy/purpledefender.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class purpledefender : MonoBehaviour
 {
-    public Animator hitted;
+    // public Animator hitted;
     
     private void OnTriggerEnter2D(Collider2D other){
         
--- Assets/Scripts/WBC/Slash.cs
+++ Assets/Scripts/WBC/Slash.cs
@@ -14,6 +14,8 @@ public class Slash : MonoBehaviour
     public GameObject thrust2;
     public GameObject thrust3;
 
+
+
     public void EndAttack(){
         gameObject.SetActive(false);
     }
@@ -36,8 +38,6 @@ public class Slash : MonoBehaviour
                 {
                     Instantiate(thrust3,other.transform.position+new Vector3(2.5f,0f,other.transform.position.z),Quaternion.identity);
                 }
-                
-                
                 StartCoroutine(FindObjectOfType<camcontroller>().CameraShakeCo(0.1f, 0.4f)); // camera shake
                 // 击退效果
                 #region 
@@ -48,9 +48,7 @@ public class Slash : MonoBehaviour
                 other.transform.position = new Vector2(other.transform.position.x + difference.x,
                                                         other.transform.position.y + difference.y);
                 #endregion
-
             }
-            
         }
     
         if(other.gameObject.tag == "boss"){ // we hit enemy
@@ -59,7 +57,15 @@ public class Slash : MonoBehaviour
             if(!enemy.isAttacked){ // 只有当敌人isAttacked为false时才能造成伤害
                 enemy.TakenDamage(attackDamage); //  给敌人造成伤害
                 DamageNum damagable = Instantiate(damageCanvas, other.transform.position, Quaternion.identity).GetComponent<DamageNum>(); //如果需要旋转的话就是quaternion.rotation 
-                damagable.ShowUIDamage(Mathf.RoundToInt(attackDamage)); // show damage  
+                damagable.ShowUIDamage(Mathf.RoundToInt(attackDamage)); // show damage
+                if(FullControl.normalorultimate==0)
+                {
+                    Instantiate(thrust2,other.transform.position,Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(thrust3,other.transform.position+new Vector3(2.5f,0f,other.transform.position.z),Quaternion.identity);
+                }
                 StartCoroutine(FindObjectOfType<camcontroller>().CameraShakeCo(0.1f, 0.4f)); // camera shake
                 #region 
                 Vector2 difference = other.transform.position - transform.position;
@@ -69,6 +75,34 @@ public class Slash : MonoBehaviour
                 other.transform.position = new Vector2(other.transform.position.x + difference.x,
                                                         other.transform.position.y + difference.y);
                 #endregion
+            }
+        }
+
+        if(other.gameObject.tag == "meatshield"){ // we hit enemy
+            attackDamage = Random.Range(minDamage, maxDamage);
+            MeatShield enemy=other.gameObject.GetComponent<MeatShield>();
+            if(!enemy.isAttacked){ // 只有当敌人isAttacked为false时才能造成伤害
+                enemy.TakenDamage(attackDamage); //  给敌人造成伤害
+                DamageNum damagable = Instantiate(damageCanvas, other.transform.position, Quaternion.identity).GetComponent<DamageNum>(); //如果需要旋转的话就是quaternion.rotation 
+                damagable.ShowUIDamage(Mathf.RoundToInt(attackDamage)); // show damage  
+                if(FullControl.normalorultimate==0)
+                {
+                    Instantiate(thrust2,other.transform.position,Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(thrust3,other.transform.position+new Vector3(2.5f,0f,other.transform.position.z),Quaternion.identity);
+                }
+                StartCoroutine(FindObjectOfType<camcontroller>().CameraShakeCo(0.1f, 0.4f)); // camera shake
+                
+                // #region 
+                // Vector2 difference = other.transform.position - transform.position;
+                // difference.Normalize();
+                // // difference = (difference / 10.0f) * hitBackDistance;
+                // difference = difference * hitBackDistance;
+                // other.transform.position = new Vector2(other.transform.position.x + difference.x,
+                //                                         other.transform.position.y + difference.y);
+                // #endregion
             }
         }
     }
