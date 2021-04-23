ceb9e03e20562317a610a1b59beaed842a866055
shuchenxin <shuchenxin@gmail.com>
2021-04-22 23:30:30 -0700

    init

---
+++ Assets/Scripts/HP/BuyHealth.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Image hpBar;
+    public Image hpEffectBar;
+    public Text glucoseAmount;
+    public Text result;
+    private Text glucoseText;
+
+    void Start()
+    {
+        hpBar.fillAmount=0.2f;
+        hpEffectBar.fillAmount=hpBar.fillAmount;
+        FullControl.hp=20;
+        FullControl.glucose=10;
+        glucoseText=GameObject.FindGameObjectWithTag("showglucose").GetComponent<Text>();
+        glucoseText.text="  "+FullControl.glucose;
+    }
+
+    private void AddHealth(float health){
+    	FullControl.hp=FullControl.hp+health;
+        if(FullControl.hp>100){
+            FullControl.hp=100;
+        }
+    	hpBar.fillAmount=FullControl.hp/100;
+        hpEffectBar.fillAmount=FullControl.hp/100;
+    }
+
+    public void TakeGlucose(){
+    	int amount = int.Parse(glucoseAmount.text);
+    	if(FullControl.glucose>=amount){
+    		FullControl.glucose=FullControl.glucose-amount;
+    		AddHealth(amount*10);
+    		glucoseText.text="  "+FullControl.glucose;
+    		result.text = "Success!";
+    	} else {
+    		result.text = "Don't have enough glucose";
+    	}
+    	
+    }
+}
---
+++ Assets/Scripts/Info/CharNav.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharNav : MonoBehaviour
+{
+    public GameObject panel1;
+    public GameObject panel2;
+    public GameObject panel3;
+
+    public void page1()
+    {
+    	panel1.SetActive(true);
+    	panel2.SetActive(false);
+    	panel3.SetActive(false);
+    }
+
+    public void page2()
+    {
+    	panel1.SetActive(false);
+    	panel2.SetActive(true);
+    	panel3.SetActive(false);
+    }
+
+    public void page3()
+    {
+    	panel1.SetActive(false);
+    	panel2.SetActive(false);
+    	panel3.SetActive(true);
+    }
+}
+
