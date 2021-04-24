55030d47dda155b92c7c978f285a40bce66137fd
unknown <larrywong9898@gmail.com>
2021-04-24 12:57:45 +0800

    update bodypedia

---
+++ Assets/Scripts/Info/EnemyNav.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyNav : MonoBehaviour
+{
+    public GameObject panel1;
+    public GameObject panel2;
+    public GameObject panel3;
+    public GameObject panel4;
+
+    public void page1()
+    {
+    	panel1.SetActive(true);
+    	panel2.SetActive(false);
+        panel3.SetActive(false);
+    	panel4.SetActive(false);
+    }
+
+    public void page2()
+    {
+    	panel1.SetActive(false);
+    	panel2.SetActive(true);
+        panel3.SetActive(false);
+    	panel4.SetActive(false);
+    }
+    public void page3()
+    {
+    	panel1.SetActive(false);
+    	panel2.SetActive(false);
+        panel3.SetActive(true);
+    	panel4.SetActive(false);
+    }
+    public void page4()
+    {
+    	panel1.SetActive(false);
+    	panel2.SetActive(false);
+        panel3.SetActive(false);
+    	panel4.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Info/MainNav.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainNav: MonoBehaviour
+{
+    public GameObject panel1;
+    public GameObject panel2;
+    public GameObject panel3;
+
+    public void page1()
+    {
+    	panel1.SetActive(true);
+    	panel2.SetActive(false);
+    	panel3.SetActive(false);
+    }
+
+    public void page2()
+    {
+    	panel1.SetActive(false);
+    	panel2.SetActive(true);
+    	panel3.SetActive(false);
+    }
+
+    public void page3()
+    {
+    	panel1.SetActive(false);
+    	panel2.SetActive(false);
+    	panel3.SetActive(true);
+    }
+}
+
